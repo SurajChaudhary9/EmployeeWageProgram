f6713d15445876004567cb0dc755219c30041b86
SurajChaudhary9 <surajchaudhary0707@gmail.com>
2022-02-05 23:59:21 +0530

    UC4 Added : Employee Part Time and Wage Using Switch Statement ( case )

--- EmployeeWageBuilder.java
+++ EmployeeWageBuilder.java
@@ -2,21 +2,32 @@ package com.emp;
 
 public class EmployeeWageBuilder 
 {
-	int IS_FULL_TIME = 1;
-	int IS_PART_TIME = 2;
-	int EMP_RATE_PER_HOUR = 20;
-	int empHrs = 0;
-	int empWage = 0;
-	
-	double empCheck = Math.floor(Math.random() * 10 ) % 3;
-	{	
-		if (empCheck == IS_FULL_TIME )
-		empHrs = 8;
+	public static void main(String[] args) {
+		final int IS_FULL_TIME = 1;
+		final int IS_PART_TIME = 2;
+		final int EMP_RATE_PER_HOUR = 20;
 		
-		else if (empCheck == IS_PART_TIME )
-		empHrs = 4;
+		int empHrs = 0;
+
+		double empCheck = Math.floor(Math.random() * 10 ) % 3;
 		
-	empWage = empHrs * EMP_RATE_PER_HOUR;
-	System.out.println("Employee Wage:" +empWage);
+		{
+		switch((int)empCheck) 
+		{
+		case IS_FULL_TIME:
+			empHrs = 8;
+			break;
+		case IS_PART_TIME:
+			empHrs = 4;
+			break;
+		default:
+			empHrs = 0;
+			break;
+	}
+}
+	{
+	int empSalary = empHrs * EMP_RATE_PER_HOUR;
+	System.out.println("EmpWage:" +empSalary);
+	}
 	}
 }
